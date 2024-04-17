0f73ad0bf51197ee03d5bc74d64b25363b0482e2
AchrafGroiez <41470d0a@gmail.com>
2024-04-17 10:05:38 -0400

    Started Bownling Setup and Main menu page

--- app/src/main/java/com/ad6f/bowling/MainActivity.java
+++ app/src/main/java/com/ad6f/bowling/MainActivity.java
@@ -2,7 +2,9 @@ package com.ad6f.bowling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void leave(View view) {
+        // To close the app but need to see
+        this.finish();
+    }
+
+    public void play(View view) {
     }
 }
