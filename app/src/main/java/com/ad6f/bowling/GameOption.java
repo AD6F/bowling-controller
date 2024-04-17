e75cc224edb76c391ce2461e748a469326a66d50
AchrafGroiez <41470d0a@gmail.com>
2024-04-17 13:56:21 -0400

    Added Back and Next button and changed round from EditText to Slider in page game_option.xml

--- app/src/main/java/com/ad6f/bowling/GameOption.java
+++ app/src/main/java/com/ad6f/bowling/GameOption.java
@@ -1,6 +1,8 @@
 package com.ad6f.bowling;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,9 @@ public class GameOption extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_option);
+    }
+
+    public void back(View view) {
+        this.startActivity(new Intent(this, MainActivity.class));
     }
 }
--- app/src/main/java/com/ad6f/bowling/MainActivity.java
+++ app/src/main/java/com/ad6f/bowling/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +14,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void leave(View view) {
-        // To close the app but need to see
+        // Not working if we open other activities
         this.finish();
     }
 
     public void play(View view) {
+        this.startActivity(new Intent(this, GameOption.class));
     }
 }
