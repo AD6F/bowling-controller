fabcd73b1645386f8195c2884fe4c9bcdb69fef8
AchrafGroiez <41470d0a@gmail.com>
2024-04-18 00:21:31 -0400

    Changed main-page style and started add player popup

--- app/src/main/java/com/ad6f/bowling/GameOption.java
+++ app/src/main/java/com/ad6f/bowling/GameOption.java
@@ -3,6 +3,7 @@ package com.ad6f.bowling;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/ad6f/bowling/MainActivity.java
+++ app/src/main/java/com/ad6f/bowling/MainActivity.java
@@ -1,12 +1,21 @@
 package com.ad6f.bowling;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.runtime.State;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> players = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +24,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void leave(View view) {
         // Not working if we open other activities
-        this.finish();
+        var alert = new AlertDialog.Builder(this);
+        var input = new EditText(this);
+        input.setSingleLine();
+        input.setHint("Player name");
+        alert.setView(input);
+
+        alert.setTitle("Add Player : ");
+        alert.setPositiveButton("Ok", (dialog, which) -> {
+            System.out.println(dialog+" "+ which);
+        });
+
+        alert.setNegativeButton("Cancel", (dialog, which) -> {
+
+        });
+        alert.show();
+
+        //this.finish();
     }
 
     public void play(View view) {
