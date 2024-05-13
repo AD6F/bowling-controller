1d14dbded8c25cfef9c03e29bdeca5b53d7a1614
AchrafGroiez <41470d0a@gmail.com>
2024-05-13 09:51:00 -0400

    Removed old cast page and putted cast button in the MainActivity

--- app/src/main/java/com/ad6f/bowling/MainActivity.java
+++ app/src/main/java/com/ad6f/bowling/MainActivity.java
@@ -1,21 +1,45 @@
 package com.ad6f.bowling;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.ad6f.bowling.cast.CastInfo;
+import com.ad6f.bowling.cast.SessionManagerListenerImpl;
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.CastSession;
+import com.google.android.gms.cast.framework.SessionManager;
+import com.google.android.gms.cast.framework.SessionManagerListener;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private CastContext castContext;
+    private SessionManager sessionManager;
+
+    private SessionManagerListener<CastSession> mSessionManagerListener =
+            new SessionManagerListenerImpl(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lier le boutton au Cast
+        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), findViewById(R.id.cast_media));
+        castContext = CastContext.getSharedInstance(getApplicationContext());
+        sessionManager = castContext.getSessionManager();
+        sessionManager.addSessionManagerListener(mSessionManagerListener, CastSession.class);
     }
 
-    public void leave(View view) {
-        // Not working if we open other activities
-        //this.finish();
-        this.startActivity(new Intent(this, CastConnect.class));
+    public void leave(View view) throws JSONException {
+        var object = new JSONObject();
+        object.put("msg", "I always comeback!");
+
+        // Get la currentSession et envoie un message
+        sessionManager.getCurrentCastSession().sendMessage(CastInfo.SETTING_NAMESPACE, object.toString());
     }
 
     public void play(View view) {
