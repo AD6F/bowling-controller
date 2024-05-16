c8554110357e8a2326c49e8dbcda29d817375a24
AchrafGroiez <41470d0a@gmail.com>
2024-05-16 12:02:05 -0400

    Added launch button in GameLoop.kt

--- app/src/main/java/com/ad6f/bowling/GameLoop.kt
+++ app/src/main/java/com/ad6f/bowling/GameLoop.kt
@@ -8,28 +8,44 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.ad6f.bowling.cast.CastInfo
 import com.ad6f.bowling.ui.theme.MyApplicationTheme
+import com.google.android.gms.cast.framework.CastContext
+import com.google.android.gms.cast.framework.CastSession
+import org.json.JSONObject
 
 class GameLoop : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val context = LocalContext.current;
+
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Pause()
+                        LaunchButton(CastContext.getSharedInstance(context).sessionManager.currentCastSession!!)
+                    }
                 }
             }
         }
@@ -37,38 +53,24 @@ class GameLoop : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun Pause() {
-    Button(onClick = { /*TODO*/ }) {
-        Text(text = "Pause")
+    SmallFloatingActionButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Default.Settings, contentDescription = "Pause")
     }
 }
 
 @Composable
-fun LaunchButton() {
-    Button(onClick = {}, modifier = Modifier.pointerInput(Unit) {
-        detectTapGestures(
-            onPress = {
+fun LaunchButton(
+    castSession: CastSession
+) {
+    Button(onClick = {
+        val jsonObject = JSONObject()
+        jsonObject.put("rotation", 35.75f)
+        jsonObject.put("force", 30.99f)
 
-            },
-        )
+        castSession.sendMessage(CastInfo.GAME_NAMESPACE, jsonObject.toString())
     }) {
         Text(text = "Launch")
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Column {
-        Pause()
-        LaunchButton()
-    }
 }
--- app/src/main/java/com/ad6f/bowling/GameSetup.kt
+++ app/src/main/java/com/ad6f/bowling/GameSetup.kt
@@ -4,20 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -25,15 +17,11 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -43,8 +31,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -164,9 +150,7 @@ fun PlayerSetup() {
 
             LazyColumn {
                 items (players) {player ->
-                    Button(
-                        onClick = {players.remove(player)},
-                    ) {
+                    Button(onClick = { players.remove(player) }) {
                         Text(player)
                     }
                 }
