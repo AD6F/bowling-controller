db3d28549606131b52c897db7d78e6d7fc663ebe
AchrafGroiez <41470d0a@gmail.com>
2024-12-31 16:10:07 -0500

    Added new house map.

--- app/src/main/java/com/ad6f/bowling/GameSetup.kt
+++ app/src/main/java/com/ad6f/bowling/GameSetup.kt
@@ -8,15 +8,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -44,7 +41,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -133,6 +129,7 @@ val mapOptions = arrayListOf(
     Map("Grimace", R.drawable.map_grimace),
     Map("Deltarune", R.drawable.map_deltarune),
     Map("House", R.drawable.map_house),
+    Map("School", R.drawable.map_school),
 )
 
 const val OPTION_TITLE = 20;
